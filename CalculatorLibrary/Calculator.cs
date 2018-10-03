ea8959ad8893d362c676f3924efda5f16aeeee9e
Victor Wolden West-Larsen <vict536f@edu.eal.dk>
2018-10-03 10:31:27 +0200

    Sum, min og max

--- CalculatorLibrary/Calculator.cs
+++ CalculatorLibrary/Calculator.cs
@@ -8,5 +8,49 @@ namespace CalculatorLibrary
 {
     public class Calculator
     {
+        public static int Sum(int[] ints)
+        {
+            int sum = 0;
+            for (int l = 0; l < ints.Length; l++)
+            {
+
+                sum = sum + ints[l];
+
+            }
+            return sum;
+
+        }
+
+        public static int Min(int[] ints)
+        {
+            int min = 0;
+            for (int j = 0; j < ints.Length; j++)
+            {
+                if (ints[j] < ints[min])
+                {
+                    min = j;
+                }
+            }
+
+            return ints[min];
+
+        }
+
+        public static int Max(int[] ints)
+        {
+            int max = 0;
+            for (int i = 0; i < ints.Length; i++)
+            {
+                if (ints[i] > ints[max])
+                {
+                    max = i;
+                }
+
+
+
+            }
+
+            return ints[max];
+        }
     }
 }
